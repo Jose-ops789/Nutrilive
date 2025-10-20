085c56cb72ba92156fd44fc78b43a343a8462e39
Jose-ops789 <josesitoa03@gmail.com>
2025-10-19 21:02:54 -0500

    primer pantallazo sin conectar (sin funcionalidad) oantalla principal actualizada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.navigation:navigation-compose:2.9.5")
 
 
 }
